97e2b290cbf25681ebd6ad5ffa03459e6e77ddce
Jane Falconer-White <jane@humanmade.co.uk>
2013-12-02 20:29:43 +0000

    Further work on home page formatting

--- wp-content/themes/kfas/kfas-single-home.php
+++ wp-content/themes/kfas/kfas-single-home.php
@@ -41,46 +41,46 @@ Template Name: KFAS Single Home Page
 		<li>No obligation to continue</li> 
 	</ul>
 	</div>
-
-	<hr>
+	<!--<hr>-->
+	<div id="break"></div>
 </section>
 <!--end of intro section-->
 						
-<!--start of WHAT section, three columns-->
+<!--start of SERVICES section, three columns-->
 <section id="services">
 
 <h2>Our Services</h2>
 
-						<div class="fourcol first what" id="what-section">
+						<div class="fourcol first services" id="services-section">
 						<h5 style="color:#cc3399;"><i class="icon-lightbulb icon-4x pull-left icon-muted" style="color:#cc3399;"></i>New Business Kick-Starter<br></h5>
 						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>There is so much to understand and get organised. I’m getting stressed. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
 						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I want to focus on my new business, I don’t want to waste time. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
 						<p>When you are just starting out there are a lot of things to take care of. We help with everything from the business plan, incorporations, payroll setup to selecting an accounts package.</p>
 						<p>Find out more about our start-up services and new business package.</p>
 						<p>
-							<a class="startup-button" href="<?php echo get_home_url(); ?>/services">Starting out   ></a>
+							<a class="startup-button" href="<?php echo get_home_url(); ?>/services">I'm starting out   ></a>
 						</p>
 						</div>
 						
-						<div class="fourcol what" id="what-section">
+						<div class="fourcol services" id="services-section">
 						<h5 style="color:#cc3399;"><i class="icon-ambulance icon-4x pull-left icon-muted" style="color:#cc3399;"></i>Finance First Aid<br></h5>
-						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>There is so much to understand and get organised. I’m getting stressed. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
-						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I want to focus on my new business, I don’t want to waste time. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
-						<p>When you are just starting out there are a lot of things to take care of. We help with everything from the business plan, incorporations, payroll setup to selecting an accounts package.</p>
-						<p>Find out more about our start-up services and new business package.</p>
+						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I’m behind on my returns and worried about getting fined. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
+						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I just don’t know how to get on top of my accounts. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
+						<p>Business has been busy and you’ve not done your books for a while. Before you know it you’ve got missing tax returns and fines. We can help you to get back on track with your accounts and avoid the stress and worry.</p>
+						<p>Find out more about our finance first aid package.</p>
 						<p>
-							<a class="startup-button" href="<?php echo get_home_url(); ?>/services">Starting out   ></a>
+							<a class="startup-button" href="<?php echo get_home_url(); ?>/services">I need urgent help   ></a>
 						</p>
 						</div>
 
-						<div class="fourcol what last" id="what-section">
-						<h5 style="color:#cc3399;"><i class="icon-cogs icon-4x pull-left icon-muted" style="color:#cc3399;"></i>Accounts Support</h5>
+						<div class="fourcol services last" id="services-section">
+						<h5 style="color:#cc3399;"><i class="icon-bar-chart icon-4x pull-left icon-muted" style="color:#cc3399;"></i>Business Support</h5>
 						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I've outgrown the way I used to do things, I need to make some changes. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
-						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I need a better grasp on my finances, once a year isn't enough. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
+						<p style="color:#336699;"><i class="icon-quote-left pull-left" style="color:#cc3399;"></i>I need some regular support with my finances and returns. <i class="icon-quote-right" style="color:#cc3399;"></i></p>
 						<p>Whether you need work out your VAT, CIS, submit your annual returns or keep on top of your accounts more regularly we can help. We can provide advice, support and services as your business grows.</p>
 						<p>Find out more about our business accounting and taxation services.</p>
 						<p>
-							<a class="startup-button" href="<?php echo get_home_url(); ?>/services">Business services   ></a>
+							<a class="startup-button" href="<?php echo get_home_url(); ?>/services">I need ongoing support   ></a>
 						</p>
 						</div>
 						
@@ -100,20 +100,43 @@ Template Name: KFAS Single Home Page
 		<a class="call-button" href="<?php echo get_home_url(); ?>/contact-us">Book your free consultation now   ></a>
 	</p>
 
-<hr>
-
 </section>
+
+<div id="break"></div>
 <!--end of WHAT section, three columns -->
 
 <!--start of WHY section, one column -->
 
-<section id ="about">
+<section id="about">
 	
-	<div class="twelvecol why">
-	<h2>About Us</h2>
+	<div class="twelvecol first about">
+	<h2>Our philosophy</h2>
+	<h3>You've got enough to worry about, so we'll fit in with you</h3>
 	<p>Because we’re real people we can be flexible. If you need a late or weekend appointment to fit in with work, we can do that. If you’ve got a certain way that you like to do your books then we can work with that. We won’t make you fit in with one way of working or a favourite software package, but can always provide any guidance and recommendations.
 	</p>
-	</div>			
+	<p>Because we’re real people we can be flexible. If you need a late or weekend appointment to fit in with work, we can do that. If you’ve got a certain way that you like to do your books then we can work with that. We won’t make you fit in with one way of working or a favourite software package, but can always provide any guidance and recommendations.
+	</p>
+	</div>	
+
+</section> 
+
+<!--end of WHY section, one columns -->
+
+<!--start of CONTACT section, two column -->
+
+<section id ="contact">
+	
+	<div class="sixcol first contact">
+	<h2>Contact us</h2>
+	[contact-form-7 id="220" title="Contact form 1"]
+	</div>
+
+	<div class="sixcol last contact">
+	<h2>Our philosophy</h2>
+	<h3>You've got enough to worry about, so we'll fit in with you</h3>
+	<p>Because we’re real people we can be flexible. If you need a late or weekend appointment to fit in with work, we can do that. If you’ve got a certain way that you like to do your books then we can work with that. We won’t make you fit in with one way of working or a favourite software package, but can always provide any guidance and recommendations.
+	</p>
+	</div>				
   
 </section> 
    				</div> <!-- end #main -->
