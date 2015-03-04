38b0cf2441bc5b96e4b4ee3a4c563314857e88c9
janefw <jane@hmn.md>
2015-03-04 20:07:48 +0000

    Interim changes
    Moved products onto front page
    Some changes to header and footer

--- wp-content/themes/kfas/footer.php
+++ wp-content/themes/kfas/footer.php
@@ -12,7 +12,7 @@
 	    	<li><a class="link" href="#services-a"><i class="icon-book"></i> Our Services</a></li>
 	        <li><a class="link" href="#about-a"><i class="fa fa-coffee"></i> About</a></li>
 	    	<li><a class="link" href="#who-a"><i class="icon-user"></i> Who</a></li>
-	    	<li><a class="link" href="<?php echo home_url(); ?>/blog"><i class="fa fa-pencil-square-o"></i> Useful</a></li>
+	    	<li><a class="link" href="<?php echo home_url(); ?>/blog"><i class="fa fa-pencil-square-o"></i> Blog</a></li>
 	        <li><a class="link" href= "#contact-a"><i class="icon-phone"></i> Contact Us</a></li>
 	        <!--<li><a class="link" href= "<?php echo home_url(); ?>/jobs"><i class="icon-envelope"></i> Jobs</a></li>-->
 	    	</ul>
@@ -25,6 +25,10 @@
 		<p>
 		<a href="http://www.cimaglobal.com/">
 			<img src="<?php echo get_template_directory_uri(); ?>/library/images/PCH_pink.jpg" alt="CIMA" width="150" height="83">
+		</a>
+		</br></br>
+		<a href="http://www.xero.com/">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/xero-logo-hires-RGB-sml.png" title="We are a Xero Partner" alt="We are a Xero Partner" width="200" height="100">
 		</a>
 		</br></br>
 		<a href="http://www.clearbooks.co.uk/">
@@ -44,9 +48,10 @@
 					
 	<nav role="navigation" class="nav service">
 	<ul class="nav">
-		<!--<li><a href="http://www.facebook.com/KFAccountingServices/"><i class="icon-facebook-sign icon-2x"></i> Facebook</a></li>-->
-		<li><a href="http://www.twitter.com/kfaccounting/" target="_blank"><i class="icon-twitter-sign icon-2x"></i> Twitter</a></li>
+		<li><a href="http://www.facebook.com/KFAccountingServices/"><i class="icon-facebook-sign icon-2x"></i> Chat on Facebook</a></li>
+		<li><a href="http://www.twitter.com/kfaccounting/" target="_blank"><i class="icon-twitter-sign icon-2x"></i> Say hi on Twitter</a></li>
 		<li><a href="https://plus.google.com/u/1/b/101856043115495106107/101856043115495106107" rel="publisher" target="_blank"><i class="fa-google-plus icon-2x"></i>Review on Google+</a></li>
+		<li><a href="https://www.linkedin.com/company/kf-accounting-services/" target="_blank"><i class="fa-linkedin-square icon-2x"></i>Connect on LinkedIn</a></li>
 	</ul>
 	</nav>
 	
